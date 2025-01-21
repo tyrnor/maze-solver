560ed6974e835300f2ccdbea71dcc68ce3ca64f9
tyrnor <carlosyanez997@gmail.com>
2025-01-21 13:23:16 +0100

    Create main window

---
+++ main.py
@@ -0,0 +1,9 @@
+from graphics import Window, Line, Point
+
+def main():
+    win = Window(800, 600)
+    l = Line(Point(50,50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_to_close()
+
+main()
