c516fb319e4210057f9076f156f4a984380f5653
tyrnor <carlosyanez997@gmail.com>
2025-01-21 14:19:34 +0100

    Add break wall recursive method

--- cell.py
+++ cell.py
@@ -1,11 +1,12 @@
 from graphics import Line, Point
 
 class Cell():
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
